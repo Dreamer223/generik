205bd18c64dc0b4668ae0f310a83c4c43d650053
Dreamer223 <drackfil@gmail.com>
2024-03-14 23:38:40 +0300

    hw

--- src/main/java/JDK/ex1/Calculate.java
+++ src/main/java/JDK/ex1/Calculate.java
@@ -1,2 +1,30 @@
-package JDK.ex1;public class Calculate {
+package JDK.ex1;
+
+public class Calculate {
+//    Написать класс Калькулятор (необобщенный), который содержит обобщенные статические методы: sum(),
+//    multiply(), divide(), subtract(). Параметры этих методов – два числа разного типа, над которыми должна
+//    быть произведена операция. Методы должны возвращать результат своей работы.
+
+    public static <T extends Number> double sum(T a, T b) {
+        return a.doubleValue() + b.doubleValue();
+    }
+
+    public static <T extends Number> double multiply(T a, T b) {
+        return a.doubleValue() * b.doubleValue();
+    }
+
+    public static <T extends Number> double divide(T a, T b) {
+        return a.doubleValue() / b.doubleValue();
+    }
+
+    public static <T extends Number> double subtract(T a, T b) {
+        return a.doubleValue() - b.doubleValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2));
+        System.out.println(multiply(1, 2));
+        System.out.println(divide(1, 2));
+        System.out.println(subtract(1, 2));
+    }
 }
--- src/main/java/JDK/ex3/Pair.java
+++ src/main/java/JDK/ex3/Pair.java
@@ -1,2 +1,31 @@
-package JDK.ex3;public class Pair {
+package JDK.ex3;
+
+public class Pair <T, S> {
+    //Напишите обобщенный класс Pair, который представляет собой пару значений разного типа.
+    // Класс должен иметь методы getFirst(), getSecond() для получения значений каждого из
+    // составляющих пары, а также переопределение метода toString(), возвращающее строковое
+    // представление пары. Работу сдать в виде ссылки на гит репозиторий.
+
+    T first;
+    S second;
+
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + first + ", " + second + ")";
+    }
+
+
 }
